0d570ac4a4058c8921253b62af58acad8cfba8b2
Ihury <ihurykc@gmail.com>
2023-10-06 22:33:42 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
-using System.Text.Json.Serialization;
-using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,9 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<OrganizadorContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
-builder.Services.AddControllers().AddJsonOptions(options =>
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
